8a23695dce2788e2b137333c4c9b038b0e1f2672
David Maddison <maddisondavid@gmail.com>
2019-02-20 10:35:07 +0000

    Tidy Up

--- multidisplay/multidisplay.py
+++ multidisplay/multidisplay.py
@@ -2,31 +2,27 @@ from microbit import *
 import radio
 
 #
-# Perform an animation across multiple Micro:bit displays... in sync
-#
-#  Main Loop:
-#     - Animate into Frame Buffer
-#     - Chop FrameBuffer up into FrameSections and distribute
-#     - Render FrameSection
-#
+# Performs an animation over several connected Micro:bit screens that self organize
 
-RENDER_MSG = "RENDER"
-NEED_NUMBER_MSG = "NEEDSCREEN"
+REQUEST_NUMBER_MSG = "REQUEST"
 ASSIGN_NUMBER_MSG = "ASSIGN"
+RENDER_MSG = "RENDER"
 
+START_SETUP_PIN = pin0
+NOTIFY_NEXT_PIN = pin1
+
+# Screen number will be -1 until assigned
+MASTER_SCREEN_NUMBER = 0
 screen_number = -1
+
+total_width = 5
 animation_buffer = []
 localBuffer = [[0] * 5 for i in range(5)]
+number_of_screens = 1
 
+# Sprite Information
 spriteX = 2
 spriteY = 1
-sprite_reversed = False
-ballDirection = -1
-
-number_of_screens = 3
-
-max_cols = 5
-
 sprite_width = 4
 sprite_height = 3
 sprite = [[3,5,9,0],
@@ -34,10 +30,11 @@ sprite = [[3,5,9,0],
           [3,5,9,0]]
 
 def initialize():
-    global is_master, screen_number
+    """Initialise the system by waiting to see if we're a Master or Slave Node"""
+    global screen_number
 
-    pin0.write_digital(0)
-    pin1.write_digital(0)
+    # Make sure our 'notification' pin is 0
+    NOTIFY_NEXT_PIN.write_digital(0)
 
     radio.config(group=1)
     radio.on()
@@ -46,84 +43,80 @@ def initialize():
     display.show("--")
     while True:
         if button_a.is_pressed():
-            screen_number = 0
-
             master_setup()
             break
 
         if pin0.read_digital() == 1:
-            # Clear radio buffer since other comms traffic will been received
+            # Other bits traffic would have been received while waiting, so clear the buffer
             while radio.receive() is not None:
                 None
 
-            radio.send(NEED_NUMBER_MSG)
+            # Ask the Master Bit for a number
+            radio.send(REQUEST_NUMBER_MSG)
 
             # Wait here for response
             number_assign = None
             while number_assign is None:
                 number_assign = radio.receive()
 
-            # Extract screen number
-            print("MSG="+number_assign)
+            # Extract screen number from message
             screen_number = int(number_assign[len(ASSIGN_NUMBER_MSG):])
-            display.show(screen_number)
 
-            # Trigger next Node
-            pin1.write_digital(1)
-
+            # Notify next bit in line
+            NOTIFY_NEXT_PIN.write_digital(1)
             break
 
         sleep(100)
 
     display.show(screen_number)
-    sleep(500)
+    sleep(1000)
 
     display.clear()
 
+
 def master_setup():
-    global number_of_screens
+    """Main loop for Master node setup that waits assigning numbers to screens when requested"""
+    global screen_number, number_of_screens
 
-    display.show("0")
+    screen_number = MASTER_SCREEN_NUMBER
+    display.show(screen_number)
 
-    # Trigger chain reaction
-    pin1.write_digital(1)
+    # Notify next bit
+    NOTIFY_NEXT_PIN.write_digital(1)
 
     next_screen = 1
 
-    print("Master Setup Start")
-    all_assigned = False
-    while not all_assigned:
+    # Keep looping until we get a SETUP request, which means we've gone through all the screens
+    while not START_SETUP_PIN.read_digital() == 1:
         screen_setup_msg = radio.receive()
 
         if screen_setup_msg is not None:
-            if screen_setup_msg == NEED_NUMBER_MSG:
+            # If this is a request for a number, send one back
+            if screen_setup_msg == REQUEST_NUMBER_MSG:
                 radio.send(ASSIGN_NUMBER_MSG + str(next_screen))
                 next_screen += 1
-
-        # We're finished if it loops around
-        all_assigned = pin0.read_digital() == 1
 
         sleep(100)
 
     number_of_screens = next_screen
-    print("Master Setup Finished")
 
 
 def create_animation_buffer():
-    global number_of_screens, animation_buffer, max_cols
+    """Creates the main animation buffer based on the number of screens"""
+    global number_of_screens, animation_buffer, total_width
 
-    max_cols = 5 * number_of_screens
-    animation_buffer = [[0] * max_cols for i in range(5)]
+    total_width = 5 * number_of_screens
+    animation_buffer = [[0] * total_width for i in range(5)]
 
 
 def animate():
     """Perform animation into the animation buffer"""
-    global spriteX, spriteY, sprite_width, sprite_height, sprite_reversed, ballDirection
+    global spriteX, spriteY, sprite_width, sprite_height
 
     # Move Sprite Along
     spriteX += 1
 
-    if spriteX > max_cols:
+    if spriteX > total_width:
         spriteX = 0 - sprite_width
 
     start_col = 0
@@ -134,8 +127,8 @@ def animate():
         start_col = spriteX * -1
 
     # If the sprite is at the end of the animation buffer, render just the part showing
-    if spriteX + sprite_width > max_cols:
-        end_col = max_cols - spriteX
+    if spriteX + sprite_width > total_width:
+        end_col = total_width - spriteX
 
     # Render sprite into animation buffer
     for row in range(0,sprite_height):
@@ -151,10 +144,11 @@ def reset_buffer():
 
 
 def distribute_screen_buffers():
-    """Break up the animation buffer into screen buffers and send to Microbits"""
+    """Break up the animation buffer into screen buffers and send to slave Micro:bits"""
     global number_of_screens, localBuffer
 
     for screen in range(0, number_of_screens):
+        # If this is for ourselves (the master) then just copy directly to local buffer
         if screen == 0:
             for row in range(0, 5):
                 for col in range(0, 5):
@@ -168,20 +162,8 @@ def distribute_screen_buffers():
             radio.send(str(screen)+screen_data)
 
 
-def trigger_render():
-    """Send out the command to render"""
-    radio.send(RENDER_MSG)
-
-
-def renderLocal():
-    """Render the local buffer data onto the screen"""
-    for row in range(0, 5):
-        for col in range(0, 5):
-            display.set_pixel(col, row, localBuffer[row][col])
-
-
 def wait_for_buffer_data():
-    """Wait for frame buffer data to be sent by master"""
+    """Called by a slave node when it needs to wait to receive the next frame information from the master"""
     data_loaded = False
     while not data_loaded:
         buffer_data = None
@@ -196,7 +178,7 @@ def wait_for_buffer_data():
 
 
 def load_local_buffer(buffer_data):
-    """Load received buffer data into our local buffer"""
+    """Decode the new frame information into the local buffer"""
     for row in range(0, 5):
         for col in range(0, 5):
             row_start = 5 * row
@@ -205,7 +187,7 @@ def load_local_buffer(buffer_data):
 
 
 def wait_for_render():
-    """Wait for render command to be sent by master """
+    """Called by slave when it needs to wait to receive the render tick"""
     render_received = False
     while not render_received:
         msg = None
@@ -213,24 +195,37 @@ def wait_for_render():
             msg = radio.receive()
 
         if msg == RENDER_MSG:
-            renderLocal()
+            render_local()
             render_received = True
+
+
+def trigger_render():
+    """Send out the command to render"""
+    radio.send(RENDER_MSG)
+
+
+def render_local():
+    """Render the local buffer data onto the screen"""
+    for row in range(0, 5):
+        for col in range(0, 5):
+            display.set_pixel(col, row, localBuffer[row][col])
 
 
 initialize()
 create_animation_buffer()
-print("Start Main Loop")
+
 while True:
-    if screen_number == 0:
+    if screen_number == MASTER_SCREEN_NUMBER:
         reset_buffer()
         animate()
-        distribute_screen_buffers()
-        sleep(5)
-        trigger_render()
 
-        renderLocal()
+        distribute_screen_buffers()
+        # Small sleep before render is triggered to give Slave Micro:bits enough time to decode screen data
+        sleep(5)
+
+        trigger_render()
+        render_local()
     else:
         wait_for_buffer_data()
         wait_for_render()
-        renderLocal()
-
+        render_local()
